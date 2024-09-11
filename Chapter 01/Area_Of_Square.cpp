d035661fef0ee4562e76971a9af769e8e87484dc
Yumna <yumnamubeen0019@gmail.com>
2024-09-11 21:59:52 +0500

    DSA with C++

---
+++ Chapter 01/Area_Of_Square.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    int a;
+    std::cout<<std::endl;
+    std::cout << "Enter area of square: "<<std::endl;
+    std::cin>> a;
+    std::cout << "Area of a square is: "<< a*a;
+    return 0;
+}
