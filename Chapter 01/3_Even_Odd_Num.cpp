ba7250e8fac1667b97df5edba693e7e164ded002
Yumna <yumnamubeen0019@gmail.com>
2024-09-11 22:42:47 +0500

    Even Odd Num

--- Chapter 01/3_Even_Odd_Num.cpp
+++ Chapter 01/3_Even_Odd_Num.cpp
@@ -1,14 +1,18 @@
-#include<iostream>
-int main(){
-    std::cout<<std::endl;
+#include <iostream>
+int main()
+{
+    std::cout << std::endl;
     int n;
-    std::cout<<"Enter a number: ";
-    std::cin>> n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
 
-    if (n%2 == 0){
-        std::cout<<"It is an Even number.";
-    }else{
-        std::cout<<"It is an Odd number.";
+    if (n % 2 == 0)
+    {
+        std::cout << "It is an Even number.";
+    }
+    else
+    {
+        std::cout << "It is an Odd number.";
     }
     return 0;
 }
---
+++ Chapter 01/4_Sum_Of_N_Nums.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main(){
+    std::cout<<std::endl;
+    int n;
+    std::cout<<
+    return 0;
+}
