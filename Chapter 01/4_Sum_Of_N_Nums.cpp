aee0a6eda8c8e1bff6d263c7afae4f5f1f607604
Yumna <yumnamubeen0019@gmail.com>
2024-09-11 22:53:56 +0500

    Sum of n Num

--- Chapter 01/4_Sum_Of_N_Nums.cpp
+++ Chapter 01/4_Sum_Of_N_Nums.cpp
@@ -1,7 +1,15 @@
-#include<iostream>
-int main(){
-    std::cout<<std::endl;
-    int n;
-    std::cout<<
+#include <iostream>
+int main()
+{
+    std::cout << std::endl;
+    int n, sum = 0;
+    std::cout << "Enter a number(n) to calculate the sum from 0 to n: ";
+    std::cin >> n;
+
+    for (int i = 0; i <= n; i++)
+    {
+        sum += i;
+    }
+    std::cout << "The sum of numbers from 0 to " << n << " is: " << sum << std::endl;
     return 0;
 }
