23b53d4ba9dbc72e43988f3761335eaca2fb84e0
Yumna <yumnamubeen0019@gmail.com>
2024-10-11 13:01:09 +0500

    5_Prime_Num

--- Chapter 01/5_Prime_Num.cpp
+++ Chapter 01/5_Prime_Num.cpp
@@ -1,24 +1,57 @@
+// #include <iostream>
+// int main()
+// {
+//     std::cout << std::endl;
+//     int n, i = 2;
+//     std::cout << "Enter a nmber: ";
+//     std::cin >> n;
+
+//     while (i <= n - 1)
+//     {
+//         if (n % i == 0)
+//         {
+//             std::cout << "It is a Not-Prime Number.";
+//             break;
+//         }
+//         else
+//         {
+//             i++;
+//         }
+        
+//     std::cout << "It is a Prime Number.";
+//     }
+//     return 0;
+// }
+
+
+
+
 #include <iostream>
 int main()
 {
     std::cout << std::endl;
     int n, i = 2;
-    std::cout << "Enter a nmber: ";
+    bool isPrime = true;  // Assuming the number is prime initially
+
+    std::cout << "Enter a number: ";
     std::cin >> n;
 
+    // Check divisibility from 2 to n-1
     while (i <= n - 1)
     {
         if (n % i == 0)
         {
-            std::cout << "It is a Not-Prime Number.";
+            isPrime = false;  // If divisible, it's not a prime number
             break;
         }
-        else
-        {
-            i++;
-        }
-        
-    std::cout << "It is a Prime Number.";
+        i++;
     }
+
+    // Print result based on isPrime flag
+    if (isPrime && n > 1)
+        std::cout << "It is a Prime Number.";
+    else
+        std::cout << "It is a not Prime Number.";
+
     return 0;
 }
